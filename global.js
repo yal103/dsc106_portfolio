bb3d1fefadb594e16b0beb3b3cbb5c3f54712375
Yangyang Liu <yal103@ucsd.edu>
2026-04-17 14:10:29 -0700

    dark mode

--- global.js
+++ global.js
@@ -49,3 +49,33 @@ for (let p of pages) {
 
     nav.append(a);
 }
+
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+        Theme:
+        <select>
+          <option value="light dark">Automatic</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+        </select>
+      </label>
+    `
+);
+
+let select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value
+});
+
+if ('colorScheme' in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
+
+
