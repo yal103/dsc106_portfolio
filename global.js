34d85bdbeeca952ac161a864759f07d8323fa82c
Yangyang Liu <yal103@ucsd.edu>
2026-04-24 11:04:42 -0700

    load github data from api

--- global.js
+++ global.js
@@ -141,3 +141,8 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
       containerElement.appendChild(article);
     }
 }
+
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData} from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
@@ -6,3 +6,31 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('yal103');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+        <h2>My GitHub Stats</h2>
+        <dl class="stats">
+            <div>
+            <dt>Followers</dt>
+            <dd>${githubData.followers}</dd>
+            </div>
+            <div>
+            <dt>Following</dt>
+            <dd>${githubData.following}</dd>
+            </div>
+            <div>
+            <dt>Public Repos</dt>
+            <dd>${githubData.public_repos}</dd>
+            </div>
+            <div>
+            <dt>Public Gists</dt>
+            <dd>${githubData.public_gists}</dd>
+            </div>
+        </dl>
+        `;
+}
