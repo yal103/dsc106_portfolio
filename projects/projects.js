7f9d79bc8e42c761604d2b85419cb62c53d98a29
Yangyang Liu <yal103@ucsd.edu>
2026-04-30 21:20:35 -0700

    improved search bar

--- projects/projects.js
+++ projects/projects.js
@@ -47,3 +47,20 @@ data.forEach((d, idx) => {
         .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
         .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener('input', (event) => {
+    // update query value
+    query = event.target.value;
+
+    // filter projects
+    let filteredProjects = projects.filter((project) => {
+        let values = Object.values(project).join('\n').toLowerCase();
+        return values.includes(query.toLowerCase());
+    });
+
+    // render filtered projects
+    renderProjects(filteredProjects, projectsContainer, 'h2');
+});
