4c2c58fe458370c8ab51083264740bc5990b1b5e
Yangyang Liu <yal103@ucsd.edu>
2026-05-08 15:08:22 -0700

    add tooltip

--- meta/main.js
+++ meta/main.js
@@ -165,7 +165,39 @@ function renderScatterPlot(data, commits) {
     .attr("cx", (d) => xScale(d.datetime))
     .attr("cy", (d) => yScale(d.hourFrac))
     .attr("r", 5)
-    .attr("fill", "steelblue");
+    .attr("fill", "steelblue")
+    .on("mouseenter", (event, commit) => {
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on("mouseleave", () => {
+      updateTooltipVisibility(false);
+    });
+}
+
+function renderTooltipContent(commit) {
+  const link = document.getElementById("commit-link");
+  const date = document.getElementById("commit-date");
+
+  if (Object.keys(commit).length === 0) return;
+
+  link.href = commit.url;
+  link.textContent = commit.id;
+  date.textContent = commit.datetime?.toLocaleString("en", {
+    dateStyle: "full",
+  });
+}
+
+function updateTooltipVisibility(isVisible) {
+  const tooltip = document.getElementById("commit-tooltip");
+  tooltip.hidden = !isVisible;
+}
+
+function updateTooltipPosition(event) {
+  const tooltip = document.getElementById("commit-tooltip");
+  tooltip.style.left = `${event.clientX}px`;
+  tooltip.style.top = `${event.clientY}px`;
 }
 
 let data = await loadData();
