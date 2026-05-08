9565372baefcc90f78172bdd9369e41eeb0cd658
Yangyang Liu <yal103@ucsd.edu>
2026-05-08 14:48:26 -0700

    visualizing time and day of commits (scatterplot)

--- meta/main.js
+++ meta/main.js
@@ -94,7 +94,82 @@ function renderCommitInfo(data, commits) {
   addStat("Most work done", maxPeriod);
 }
 
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .style("overflow", "visible");
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  // Update scales with new ranges
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  // Add gridlines BEFORE the axes
+  const gridlines = svg
+    .append("g")
+    .attr("class", "gridlines")
+    .attr("transform", `translate(${usableArea.left}, 0)`);
+
+  // Create gridlines as an axis with no labels and full-width ticks
+  gridlines.call(
+    d3.axisLeft(yScale).tickFormat("").tickSize(-usableArea.width)
+  );
+
+  // Create the axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, "0") + ":00");
+
+  // Add X axis
+  svg
+    .append("g")
+    .attr("transform", `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  // Add Y axis
+  svg
+    .append("g")
+    .attr("transform", `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+  const dots = svg.append("g").attr("class", "dots");
+
+  dots
+    .selectAll("circle")
+    .data(commits)
+    .join("circle")
+    .attr("cx", (d) => xScale(d.datetime))
+    .attr("cy", (d) => yScale(d.hourFrac))
+    .attr("r", 5)
+    .attr("fill", "steelblue");
+}
+
 let data = await loadData();
 let commits = processCommits(data);
 
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
